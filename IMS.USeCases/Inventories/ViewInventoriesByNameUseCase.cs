59d36847d16ab3b1df057bfefdfc9387a98125e3
hgirish <girish@post.com>
2023-02-16 20:08:01 -0800

    View Inventories by name use case

---
+++ IMS.USeCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,18 @@
+﻿using IMS.CoreBusiness;
+using IMS.USeCases.Inventories.Interfaces;
+using IMS.USeCases.PluginInterfaces;
+
+namespace IMS.USeCases.Inventories;
+public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+{
+    private readonly IInventoryRepository _inventoryRepository;
+
+    public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+    {
+        _inventoryRepository = inventoryRepository;
+    }
+    public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+    {
+        return await _inventoryRepository.GetInventoriesByNameAsync(name);
+    }
+}
